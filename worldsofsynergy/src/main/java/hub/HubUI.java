3e55f4c480cde375cf8a3a7f172eda2edda50c04
Dr. Stefan Reisner <stefan.reisner@syngenio.de>
2015-12-28 02:55:51 +0100

    commented-out code deleted

--- worldsofsynergy/src/main/java/hub/HubUI.java
+++ worldsofsynergy/src/main/java/hub/HubUI.java
@@ -44,52 +44,5 @@ public class HubUI extends UI
         SynergyView synergyView = new SynergyView(new VerticalSynergyLayout.NestedFactory(), WorldHelper.getWorldsNavigation());
         layout.addComponent(synergyView);
         
-//        Table table = new Table("Worlds of Synergy", WorldHelper.getWorlds());
-//        table.addGeneratedColumn("description", new ColumnGenerator() {
-//
-//            @Override
-//            public Object generateCell(Table source, Object itemId, Object columnId)
-//            {
-//                String text = (String) source.getContainerDataSource().getItem(itemId).getItemProperty("description").getValue();
-//                return new Label(text, ContentMode.HTML);
-//            }
-//            
-//        });
-//        table.setVisibleColumns("name", "description");
-//        table.setSelectable(true);
-//        table.setImmediate(true);
-//        table.addValueChangeListener(new ValueChangeListener() {
-//        @Override
-//        public void valueChange(ValueChangeEvent event)
-//        {
-//            WorldBean bean = ((BeanItem<WorldBean>)table.getItem(event.getProperty().getValue())).getBean();
-//            if (bean.getPath() != null) {
-//                String contextPath = VaadinServlet.getCurrent().getServletContext().getContextPath();
-//                getPage().setLocation(contextPath+bean.getPath());
-//            }
-//        }
-//        });
-//        table.sort(new Object[] {"name"}, new boolean[] {true});
-//        layout.addComponent(table);
-        
-//        Grid grid = new Grid("Worlds of Synergy", WorldHelper.getWorlds());
-//        grid.setColumnOrder("name", "description");
-//        grid.setSortOrder(Lists.asList(new SortOrder("name", SortDirection.ASCENDING), new SortOrder[] {}));
-//        grid.getColumn("description").setRenderer(new HtmlRenderer());
-//        grid.getColumn("description").setExpandRatio(1);
-//        grid.addSelectionListener(new SelectionListener() {
-//
-//            @Override
-//            public void select(SelectionEvent event)
-//            {
-//                WorldBean bean = (WorldBean) grid.getSelectedRow();
-//                if (bean.getPath() != null) {
-//                    String contextPath = VaadinServlet.getCurrent().getServletContext().getContextPath();
-//                    getPage().setLocation(contextPath+bean.getPath());
-//                }
-//            }
-//        });
-//        grid.setSizeFull();
-//        layout.addComponent(grid);
     }
 }
